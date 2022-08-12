4119ef69ac5143884cedace92dd5cb669f717c32
Jose Guzman <chato4010@gmail.com>
2022-08-12 08:50:34 -0500

    Readygooo

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
-'use strict';
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-var path = require('path');
-var http = require('http');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
-var oas3Tools = require('oas3-tools');
-var serverPort = 8080;
+app
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use(cors())
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use('/', require('./routes'));
 
-// swaggerRouter configuration
-var options = {
-    routing: {
-        controllers: path.join(__dirname, './controllers')
-    },
-};
 
-var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
-var app = expressAppConfig.getApp();
-
-// Initialize the Swagger middleware
-http.createServer(app).listen(process.env.PORT || 5000, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
-
